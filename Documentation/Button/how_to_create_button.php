8c026a310c3c3d1814f90949dd0f7dc6c76bc3e8
Gurudev <gurudev@VGT-C495.local>
2020-06-27 17:26:23 +0530

    yajanframework documentation file add

---
+++ Documentation/Button/how_to_create_button.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    </head>
+    <style>
+
+    </style>
+     
+    <body>
+        <div> 
+            <!--how to create button in yajan -->
+            <?php
+                $btn= new Button("test");//
+                $btn->setValue("button");//show is button text 
+	            $btn->rander();//this function use show front web page this function is not use that is not show webpage
+
+            ?>
+        </div>
+    </body>   
+</html>
